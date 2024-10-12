0abf3efb8bae888dd246d41064623b44620ad530
TNejk <100270199+TNejk@users.noreply.github.com>
2024-10-13 01:52:46 +0200

    0.2

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ topics = [
 
 # Database connection parameters
 db_connection = None
+cursor = None
 db = "postgres"
 user = "postgres"
 host = "localhost"
 password = ""  # Insert your password
 db_port = 5433  # Outside port
-
 
 def on_connect(client, userdata, flags, reason_code, properties=None):
     if reason_code == 0:
@@ -36,13 +36,17 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 
 
 def on_message(client, userdata, msg, properties=None):
+    global db_connection, cursor
     try:
-        data = msg.payload.decode("utf-8")
-        m_decoded = json.loads(data)
+        recieved = msg.payload.decode("utf-8")
+        m_decoded = json.loads(recieved)
+        data = json.dumps(m_decoded)
         topic = msg.topic
-        with open("temp_readings.txt", "a") as f:
-            f.write(topic+": "+json.dumps(m_decoded) + "\n")
+        write = f"INSERT INTO mqtt_data (topic, data) VALUES ('{topic}', '{data}')"
+        cursor.execute(write)
+        db_connection.commit()
     except (json.JSONDecodeError, IOError) as e:
+        db_connection.rollback()
         with open("error_logs.txt", "a") as error_file:
             error_file.write(f"{datetime.datetime.now()}; Error: {str(e)}\n")
 
@@ -55,39 +59,34 @@ def wait_for_mqtt():
 
 
 def db_connect():
-    global db_connection
+    global db_connection, cursor
     try:
         db_connection = psycopg2.connect(database=db, user=user, password=password, host=host, port=db_port)
+        cursor = db_connection.cursor()
+        with open("logs.txt", "a") as f:
+            f.write(f"{datetime.datetime.now()}; Connected to the database!\n")
     except psycopg2.Error as e:
         with open("logs.txt", "a") as f:
             f.write(f"{datetime.datetime.now()}; Connection failed with result code {str(e)}\n")
 
+# db connect
+db_connect()
 
 # MQTT connect
 mqtt_connected = False
+client = mqtt.Client(client_id="localny_sniffer", reconnect_on_failure=True)
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker, mqtt_port)
+client.loop_start()
+wait_for_mqtt()
+
 try:
-    # Create the client instance without specifying the protocol
-    client = mqtt.Client(client_id="localny_sniffer", reconnect_on_failure=True)
-
-    # Set the callbacks
-    client.on_connect = on_connect
-    client.on_message = on_message
-
-    # Connect to the MQTT broker
-    client.connect(broker, mqtt_port)
-
-    # Start the client loop in a separate thread
-    client.loop_start()
-
-    wait_for_mqtt()  # Wait for the connection to establish
-
-    try:
-        while True:
-            pass
-    except KeyboardInterrupt:
-        print("Exiting")
-        client.disconnect()
-        client.loop_stop()
-except Exception as e:
-    with open("wtf.txt", "a") as f:
-        f.write(f"Error: {str(e)}\n")
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Exiting")
+    client.disconnect()
+    client.loop_stop()
+    cursor.close()
+    db_connection.close()
